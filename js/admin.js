f496bc3b13cf7dbdeb8df6ea3574cd4c86db9037
antonio <antonio.cortes.sotelo@gmail.com>
2023-07-05 19:50:57 -0400

    add chart cli vent

--- js/admin.js
+++ js/admin.js
@@ -13,142 +13,6 @@ function mostrarDiaHora() {
 setInterval(mostrarDiaHora, 1000);
 
 
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var data = {
-  "labels": [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio"
-  ],
-  "datasets": [
-    {
-      "label": "2023",
-      "backgroundColor": "#ffffff",
-      "fill": false,
-      "data": [
-        "4200000",
-        "4600000",
-        "3200000",
-        "3900000",
-        "4100000",
-        "3800000",
-        "3650000"
-      ],
-      "borderWidth": "1",
-      "pointBackgroundColor": "#ffffff",
-      "borderColor": "#000000",
-      "pointBorderColor": "#000000",
-      "pointHoverBackgroundColor": "#ffffff",
-      "pointHoverBorderColor": "#000000"
-    }
-  ]
-};
-var options = {
-  "title": {
-    "display": false,
-    "text": "Ad Revenue Comparison 2014-2015",
-    "position": "bottom",
-    "fullWidth": true,
-    "fontColor": "#aa7942",
-    "fontSize": 16
-  },
-  "legend": {
-    "display": true,
-    "fullWidth": true,
-    "position": "top",
-    "labels": {
-      "fontFamily": ""
-    }
-  },
-  "scales": {
-    "yAxes": [
-      {
-        "ticks": {
-          "beginAtZero": true,
-          "display": true
-        },
-        "gridLines": {
-          "display": true,
-          "lineWidth": 2,
-          "drawOnChartArea": true,
-          "drawTicks": true,
-          "tickMarkLength": 1,
-          "offsetGridLines": true,
-          "zeroLineColor": "#a6a6a6",
-          "color": "#d6d6d6",
-          "zeroLineWidth": 2
-        },
-        "scaleLabel": {
-          "display": true,
-          "labelString": "CLP"
-        },
-        "display": true
-      }
-    ],
-    "xAxes": {
-      "0": {
-        "ticks": {
-          "display": true,
-          "fontSize": 14,
-          "fontStyle": "italic",
-          "fontColor": "#000000",
-          "beginAtZero": false
-        },
-        "display": true,
-        "gridLines": {
-          "display": true,
-          "lineWidth": 1,
-          "drawOnChartArea": false,
-          "drawTicks": true,
-          "tickMarkLength": 5,
-          "zeroLineWidth": 1,
-          "offsetGridLines": false,
-          "color": "#b3b3b3",
-          "zeroLineColor": "#cccccc"
-        },
-        "scaleLabel": {
-          "fontSize": 16,
-          "display": true,
-          "fontStyle": "normal",
-          "fontColor": "#000000"
-        },
-        "type": "category",
-        "position": "top"
-      }
-    }
-  },
-  "tooltips": {
-    "enabled": false,
-    "mode": "label",
-    "caretSize": 10,
-    "backgroundColor": "#454545"
-  },
-  "elements": {
-    "arc": {
-      "backgroundColor": "#ffffff"
-    },
-    "line": {
-      "backgroundColor": "#ffffff",
-      "lineTension": 0,
-      "borderColor": "#ffffff"
-    }
-  },
-  "animation": {
-    "duration": ""
-  }
-};
-
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: data,
-  options: options
-});
-
-
 function changeView(view) {
   var elements = document.getElementsByTagName("section");
   for (var i = 0; i < elements.length; i++) {
@@ -163,7 +27,7 @@ var estadoJornada = false;
 function switchJornada() {
   var estado = document.getElementById("semaforo-jornada");
   if (confirm('Esta seguro que quiere cambiar el estado de la jornada?')) {
-    if (estadoJornada==true) {
+    if (estadoJornada == true) {
       estado.innerHTML = '<i class="fa-solid fa-circle" style="color: #c01c28;"></i>';
       estadoJornada = false;
     } else {
@@ -173,3 +37,83 @@ function switchJornada() {
   }
 
 }
+const menuPizzeria = [
+  {
+    id: 1,
+    tipo: "pizza",
+    nombre: "Pizza Margarita",
+    precio: 8990,
+    catalogo: true,
+    imagen: "https://cdn.tictuk.com/38852cbb-9318-1769-8aeb-78d1d2dd1443/85885f88-3429-57e2-54d7-78bd9cbe7319.jpeg?a=fcc2b78b-a9a6-4fa1-a80b-5dd555bc55b9",
+    enPromocion: false
+  },
+  {
+    id: 2,
+    tipo: "pizza",
+    nombre: "Pizza Pepperoni",
+    precio: 9990,
+    catalogo: true,
+    imagen: "https://cdn.tictuk.com/38852cbb-9318-1769-8aeb-78d1d2dd1443/9808e35f-ce05-7906-b362-6fe0c817b5a4.jpeg?a=694feb99-5282-4f53-701e-1dcb6abee4b9",
+    enPromocion: true
+  },
+  {
+    id: 3,
+    tipo: "pizza",
+    nombre: "Pizza Hawaiana",
+    precio: 10990,
+    catalogo: true,
+    imagen: "https://cdn.tictuk.com/38852cbb-9318-1769-8aeb-78d1d2dd1443/85885f88-3429-57e2-54d7-78bd9cbe7319.jpeg?a=fcc2b78b-a9a6-4fa1-a80b-5dd555bc55b9",
+    enPromocion: false
+  },
+  {
+    id: 4,
+    tipo: "pizza",
+    nombre: "Pepperoni",
+    precio: 75000,
+    catalogo: false,
+    imagen: "https://www.los-almendros.com.ar/shop/wp-content/uploads/peperoni_2-1.jpg",
+    enPromocion: false
+  }
+
+];
+
+// Función para crear una tarjeta de producto
+function crearTarjetaProducto(item) {
+  const card = document.createElement("div");
+  card.classList.add('card-inventario');
+  const imagen = document.createElement('img');
+  imagen.src = item.imagen;
+  imagen.alt = item.nombre;
+  imagen.style.width = '50px';
+  card.appendChild(imagen);
+
+  const nombre = document.createElement("h4");
+  nombre.textContent = item.nombre;
+  nombre.classList.add('titulo')
+  card.appendChild(nombre);
+
+  const precio = document.createElement("p");
+  precio.classList.add('precio');
+  precio.textContent = `$${item.precio.toFixed(0)}`;
+  card.appendChild(precio);
+
+  const btn = document.createElement("button");
+  btn.textContent = "Editar";
+  card.appendChild(btn);
+  return card;
+}
+
+var containerInventario = document.getElementById("container-inventario");
+var containerCatalogo = document.getElementById("container-catalogo");
+menuPizzeria.forEach(element => {
+  if (element.catalogo == true) {
+    containerCatalogo.appendChild(crearTarjetaProducto(element));
+  }
+  else {
+    containerInventario.appendChild(crearTarjetaProducto(element));
+
+  }
+}
+
+)
+
